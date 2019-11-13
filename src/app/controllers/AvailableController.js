932b9eff7848c8043c6ad689069d9896c9cd3103
rodrigorgtic <rodrigo.rgtic@gmail.com>
2019-11-13 07:22:48 -0300

    fix: list available hours

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -21,7 +21,7 @@ class AvailableController {
     // Tentando a conversao para inteiro.
     const searchDate = Number(date);
 
-    const appointments = await Appointment.finddAll({
+    const appointments = await Appointment.findAll({
       where: {
         provider_id: req.params.providerId,
         canceled_at: null,
